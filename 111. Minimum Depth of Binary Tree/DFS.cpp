19efb432009e93ba909a7ab7d77a1f7ff3bcb81c
Pin Yuan Wang <56119216+snowflake1201@users.noreply.github.com>
2022-07-04 20:28:52 +0800

    Create DFS.cpp

---
+++ 111. Minimum Depth of Binary Tree/DFS.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return NULL;
+        int l = minDepth(root->left);
+        int r = minDepth(root->right);
+        if(l == NULL) return r + 1;
+        if(r == NULL) return l + 1;
+        return min(l, r) + 1;
+    }
+};
