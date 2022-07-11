706183c0e4fa8bd59b5723e1d48b6a33ee3ff90c
Pin Yuan Wang <56119216+snowflake1201@users.noreply.github.com>
2022-07-12 04:31:53 +0800

    Create BFS.cpp

--- 111. Minimum Depth of Binary Tree/BFS.cpp
+++ 111. Minimum Depth of Binary Tree/BFS.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int minDepth(TreeNode* root){
         int depth = 0;
-        if(root == NULL) return NULL;
+        if(root == NULL) return 0;
         queue<TreeNode* > q;
         q.push(root);
         while(!q.empty()){
