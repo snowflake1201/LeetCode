82e73251ffcd92244332a4df34779bf8e3a1ac5a
Pin Yuan Wang <56119216+snowflake1201@users.noreply.github.com>
2022-10-23 17:36:51 +0900

    Create answer.cpp

---
+++ 406. Queue Reconstruction by Height/answer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b) {
+        if(a[0] == b[0]) return a[1] < b[1];
+        return a[0] > b[0];
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), comp);
+        vector<vector<int>> ans;
+        for(auto person : people) {
+            ans.insert(ans.begin()+person[1], person);
+        }
+        return ans;
+    }
+};
